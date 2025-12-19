6bae6c880d129a7ee67ed7704ad3c7f69b533e22
Niko Komninos <nk@nikokomninos.com>
2025-12-19 02:36:43 -0500

    .

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -16,7 +16,7 @@ export const pageRequiresAuth = async () => {
   const cookieStore = await cookies();
   const token = cookieStore.get("access_token")?.value;
 
-  if (!token) redirect("/login");
+  //if (!token) redirect("/login");
 
   try {
     const res = await fetch(
